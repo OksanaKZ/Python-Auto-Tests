218f0cb467f3fb312da658c8697e96a147f5cfa1
Oksana Kozlyuk <oksanakozlyuk@gmail.com>
2024-05-28 11:01:16 +0300

    Update Task №1.6.py

--- Task №1.6.py
+++ Task №1.6.py
@@ -1,6 +1,5 @@
 #Задание: поиск элементов с помощью Selenium
 #Вам нужно открыть страницу по ссылке и заполнить форму на этой странице с помощью Selenium. 
-#Если всё сделано правильно, то вы увидите окно с проверочным кодом. Это число вам нужно ввести в качестве ответа в этой задаче.
 
 from selenium import webdriver
 from selenium.webdriver.common.by import By
@@ -29,4 +28,36 @@ finally:
     # закрываем браузер после всех манипуляций
     browser.quit()
 
-# не забываем оставить пустую строку в конце файла
+
+#Задание: поиск элемента по тексту в ссылке
+#В этой задаче мы попробуем искать элементы по тексту ссылки, для этого воспользуемся методом find_element_by_link_text.
+
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import time 
+import math
+
+link = "http://suninjuly.github.io/find_link_text"
+
+try:
+    browser = webdriver.Chrome()
+    browser.get(link)
+    link = browser.find_element(By.LINK_TEXT, str(math.ceil(math.pow(math.pi, math.e)*10000)))
+    link.click()
+    input1 = browser.find_element(By.TAG_NAME, "input")
+    input1.send_keys("Ivan")
+    input2 = browser.find_element(By.NAME, "last_name")
+    input2.send_keys("Petrov")
+    input3 = browser.find_element(By.CLASS_NAME, "city")
+    input3.send_keys("Smolensk")
+    input4 = browser.find_element(By.ID, "country")
+    input4.send_keys("Russia")
+    button = browser.find_element(By.CSS_SELECTOR, "button.btn")
+    button.click()
+
+finally:
+    # успеваем скопировать код за 30 секунд
+    time.sleep(30)
+    # закрываем браузер после всех манипуляций
+    browser.quit()
+
