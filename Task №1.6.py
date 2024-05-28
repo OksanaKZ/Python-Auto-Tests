4e1e0a1151edbd51e1eb09493baa9596f4813682
Oksana Kozlyuk <oksanakozlyuk@gmail.com>
2024-05-28 11:22:03 +0300

    Update Task №1.6.py

--- Task №1.6.py
+++ Task №1.6.py
@@ -1,5 +1,4 @@
 #Задание: поиск элементов с помощью Selenium
-#Вам нужно открыть страницу по ссылке и заполнить форму на этой странице с помощью Selenium. 
 
 from selenium import webdriver
 from selenium.webdriver.common.by import By
@@ -23,14 +22,11 @@ try:
     button.click()
 
 finally:
-    # успеваем скопировать код за 30 секунд
     time.sleep(30)
-    # закрываем браузер после всех манипуляций
     browser.quit()
 
 
-#Задание: поиск элемента по тексту в ссылке
-#В этой задаче мы попробуем искать элементы по тексту ссылки, для этого воспользуемся методом find_element_by_link_text.
+#Задание: поиск элемента по тексту в ссылке (метод find_element_by_link_text)
 
 from selenium import webdriver
 from selenium.webdriver.common.by import By
@@ -56,8 +52,25 @@ try:
     button.click()
 
 finally:
-    # успеваем скопировать код за 30 секунд
     time.sleep(30)
-    # закрываем браузер после всех манипуляций
     browser.quit()
 
+#Задание: использование метода find_elements
+
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import time
+
+try:
+    browser = webdriver.Chrome()
+    browser.get("http://suninjuly.github.io/huge_form.html")
+    elements = browser.find_elements(By.TAG_NAME, "input")
+    for element in elements:
+        element.send_keys("1")
+
+    button = browser.find_element(By.CSS_SELECTOR, "button.btn")
+    button.click()
+
+finally:
+    time.sleep(30)
+    browser.quit()
