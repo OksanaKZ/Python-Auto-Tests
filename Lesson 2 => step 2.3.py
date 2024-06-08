cb8987aa6de6b136a9cd8a80ea9b002c77b6aa93
Oksana Kozlyuk <oksanakozlyuk@gmail.com>
2024-06-08 09:07:14 +0300

    Create Lesson 2 => step 2.3.py

---
+++ Lesson 2 => step 2.3.py
@@ -0,0 +1,35 @@
+#Задание: принимаем alert
+
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import time 
+import math
+
+def calc(x):
+  return str(math.log(abs(12*math.sin(int(x)))))
+
+link = "http://suninjuly.github.io/alert_accept.html"
+
+try:
+    browser = webdriver.Chrome()
+    browser.get(link)
+
+    button = browser.find_element(By.CLASS_NAME, "btn")
+    button.click()
+
+    confirm = browser.switch_to.alert
+    confirm.accept()
+
+    x_element = browser.find_element(By.ID, "input_value")
+    x = x_element.text
+    y = calc(x)
+
+    input = browser.find_element(By.ID, "answer")
+    input.send_keys(y)
+
+    button = browser.find_element(By.CSS_SELECTOR, "button.btn")
+    button.click()
+
+finally:
+    time.sleep(30)
+    browser.quit()
