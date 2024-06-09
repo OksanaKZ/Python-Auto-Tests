f91a2a64652c493be0b184a9b861eb5b65a419ff
Oksana Kozlyuk <oksanakozlyuk@gmail.com>
2024-06-09 19:11:20 +0300

    Create Lesson 3 => step 3.2.py

---
+++ Lesson 3 => step 3.2.py
@@ -0,0 +1,4 @@
+# Задание: составные сообщения об ошибках
+
+def test_input_text(expected_result, actual_result):
+    assert actual_result == expected_result, f"expected {expected_result}, got {actual_result}"
